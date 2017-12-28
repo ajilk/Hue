55e6d3b889da188631a8746e2e517b50d60005aa
AzimeX <ajilkhomov@gmail.com>
2017-12-28 11:50:26 -0500

    Modes fixed, Guide added

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # Hue  <img src="Logo.png" width="70" />
 An application demonstrating color capabilities of the ncurses library 
 
+Colors are read from a file which offers the advantage of changing up
+the sequence of colors in any way desired.
+Looking at the source code it is visibile that flexibilty was the main key 
+behind the purpose of the project. For example, different sizes of rectangles can be set up and printed 
+multiple times at different coordinates. 
+Current project only demonstrates one shpe to view 
+series of colors but new shapes can be added and managed easily.
+
+
+## To run the program <\br>
+Make sure NUCRSES is installed and set up <\br>
+clone and run make <\br>
+the run it by typing ./color <\br>
+### To change modes  <\br>
+H - Right to Left 
+J - Top to Bottom
+K - Bottom to Top
+L - Left to Right
+
+
+## Observations 
+There were limitations. But it is not too much of a limitation but something to notice.
+There can only be ONE DEFINITION of color at a time. Lets say you redefine color red and print something 
+using that color, the moment you redefine the red, everything that was printed in red will be updated
+to the new definition of red, which is sometimes be something you do not want. In order to lessen this
+limitation, I used 6 different colors and redefined them, so they won't affect each other. In other
+I was able to get 6 shades of one color in one time. Although, this project really doesn't push
+the capabilities to the limits, it demonstrates what is possible. 
+
+
 ---
 ### Hue is part of [Concept](https://github.com/azimex/Concept) Series.
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <vector>
+#include <string>
 #include "Color.h"
 #include "Screen.h"
 #include "Rectangle.h"
@@ -12,14 +13,15 @@ int main(){
 	int centerX = screen.getColumns()/2;
 	int centerY = screen.getRows()/2;
 	ifstream infile; 
-	infile.open("../files/3.txt");
+	string filename = "../files/3.txt";
+	infile.open(filename);
 	if(!infile.is_open()){
 		printw("ERROR: Unable to open file");
 		screen.~Screen();
 		exit(EXIT_FAILURE);
 	}
-	int LENGTH = 8;
-	int WIDTH = 16;
+	int LENGTH = 10;
+	int WIDTH = 21;
 	vector< vector<Rectangle> > rectangle;
 	vector< vector<Coordinate> > coordinate;
 	for(int i=0; i<LENGTH; i++){
@@ -42,184 +44,70 @@ int main(){
 	init_pair(6, COLOR_CYAN,  COLOR_CYAN);
 	init_pair(7, COLOR_WHITE, COLOR_WHITE);	
 
-	int mode = 49;
+	int mode;
 	int pairNum = 1;
 	bool running = true;
 	while(running){
 		while(!screen.kbhit()){
-			//infile.seekg(0) won't work as intended. So file pointer is reset by closing
-			// and repopening the file
-			if(infile.eof()) {
+			infile >> color.r >> color.g >> color.b; 
+			//File has reached end of file. So random numbers are read which are thrown away
+			if(color.r > 255 || color.b > 255 || color.b > 255){
+				//infile.seekg(0) won't work as intended. So file pointer is reset by closing
+				// and repopening the file <-- BAD IDEA
 				infile.close(); 
-				infile.open("../files/3.txt");
+				infile.open(filename);
+				infile >> color.r >> color.g >> color.b; 
 			}
-			infile >> color.r >> color.g >> color.b;
 			color.convert();
+			//Notice pair num acts as a color identifier.
+			//Ex. COLOR_WHITE = 7 Pair = 7. Intended.
 			init_color(pairNum,  color.r, color.g, color.b);	
-
 			switch(mode){
-				case 49: 
+				//Top to bottom
+				case 'j':
+				case 'J': 
 					for(int i=0; i<LENGTH; i++){
 						for(int j=0; j<WIDTH; j++){
 							rectangle[i][j].print(screen, coordinate[i][j], pairNum);
-						}napms(100);
+						}napms(50);
 					}break;
-				case 50:
+				//Bottom to Top
+				case 'k':
+				case 'K':
 					for(int i=LENGTH-1; i>=0; i--){
 						for(int j=WIDTH-1; j>=0; j--){
 							rectangle[i][j].print(screen, coordinate[i][j], pairNum);
-						}napms(100);
+						}napms(50);
 					}break;	
+				//Left to Right
+				case 'l':
+				case 'L':
+					for(int j=0; j<WIDTH; j++){
+						for(int i=0; i<LENGTH; i++){
+							rectangle[i][j].print(screen, coordinate[i][j], pairNum);
+						}napms(25);
+					}break;	
+				//Right to Left
+				case 'h':
+				case 'H':
+					for(int j=WIDTH-1; j>=0; j--){
+						for(int i=LENGTH-1; i>=0; i--){
+							rectangle[i][j].print(screen, coordinate[i][j], pairNum);
+						}napms(25);
+					}break;	
+				//Front to back	
 				default:	
 					for(int i=0; i<LENGTH; i++){
 						for(int j=0; j<WIDTH; j++){
 							rectangle[i][j].print(screen, coordinate[i][j], pairNum);
 						}
-					}napms(100); break;
+					}napms(200); break;
 
-			}
-			if(pairNum >= 7) pairNum=1; else pairNum++; 
+			}if(pairNum >= 7) pairNum=1; else pairNum++; 
 		}
-
 		mode = getch();
 		if(mode == 'x' || mode == 'X')
 			running = false;
 	}
-	
-
-/*
-	
-	for(int i=0; i<length; i++){
-		for(int j=0; j<width; j++){
-			coordinate[i][j].setX(centerX-5+j);
-			coordinate[i][j].setY(centerY-2+i);
-		}
-	}
-	Color color;
-	int pairNum = 0;
-		
-	
-	int mode = 49;
-	while(mode != 'X' && mode != 'x'){	
-		while(!screen.kbhit()){
-			switch(mode){
-				case 49:
-					for(int i=0; i<length; i++){
-						infile >> color.r >> color.g >> color.b; color.convert(); 
-						init_color(i, color.r, color.g, color.b);
-						for(int j=0; j<width; j++){
-							rectangle[i][j].print(screen, coordinate[i][j], pairNum); 
-						} 
-					 	pairNum++; napms(500);
-					}
-					pairNum = 0; infile.close(); infile.open("../files/3.txt");
-					for(int i=length-1; i>=0; i--){
-						infile >> color.r >> color.g >> color.b; color.convert();
-						init_color(i, color.r, color.g, color.b);
-						for(int j=width-1; j>=0; j--){
-							rectangle[i][j].print(screen, coordinate[i][j], pairNum); 
-						} 
-					 	pairNum++; napms(500);
-					}
-
-							
-
-
-					break; 
-				case 50: break;
-				default: break;
-			}
-		}
-		mode = getch();
-	}
-
-*/
-
-
-
-
-
-/*
-	Color colors[6];
-	int mode;
-	int i=0;	
-	infile >> mode;
-	while(!infile.eof() && i<6){
-		infile >> colors[i].r; 
-		infile >> colors[i].g;
-		infile >> colors[i].b;
-		colors[i].convert();
-		i++;
-	}
-	
-	Coordinate center(screen.getRows()/2, screen.getColumns()/2);
-	Rectangle r1(4, 10);Coordinate cr1(center.getY()-r1.getLength()/2, center.getX()-r1.getWidth()/2);
-	
-	init_pair(2, COLOR_BLACK, COLOR_BLACK);
-	int choice=1;
-	while(!screen.kbhit()){
-		switch(choice){
-			case 1: {
-				if(i >= 6) choice = 2;
-				else i++;
-			} break;
-			
-			case 2: {
-				if(i <=0) choice = 1;
-				else i--;
-			} break;	
-		}
-		init_color(COLOR_BLACK, colors[i].r, colors[i].g, colors[i].b);
-		r1.print(screen, cr1, 2);
-		napms(100);
-	}
-*/
-
-/*
-	Rectangle r2(1,11); Coordinate cr2(center.getY()+1, center.getX()-r2.getWidth()/2);
-	Rectangle r3(1,11); Coordinate cr3(center.getY()+2, center.getX()-r3.getWidth()/2);	
-	Rectangle r4(1,11); Coordinate cr4(center.getY()+3, center.getX()-r4.getWidth()/2);
-
-	init_pair(1, COLOR_WHITE, COLOR_WHITE);
-	init_pair(2, COLOR_BLUE, COLOR_BLUE);
-	init_pair(3, COLOR_RED, COLOR_RED);
-	init_pair(4, COLOR_YELLOW, COLOR_YELLOW);
-
-	int choice=1;
-	while(!screen.kbhit()){
-		switch(choice){
-			case 1: {
-				for(int i=20; i<1000; i++){
-					napms(1);	
-					init_color(COLOR_WHITE, i,i,i);
-					init_color(COLOR_BLUE, i-20, i-20, i-20);
-					init_color(COLOR_RED, i-40, i-40, i-40);
-					init_color(COLOR_YELLOW, i-60, i-60, i-60);
-	
-					r1.print(screen, cr1, 1);
-					r2.print(screen, cr2, 2);
-					r3.print(screen, cr3, 3);
-					r4.print(screen, cr4, 4);
-	 			}
-			} choice=2; break; 
-		
-			case 2: {
-				for(int i=1000; i>20; i--){
-					napms(1);	
-	  				init_color(COLOR_WHITE, i,i,i);
-					init_color(COLOR_BLUE, i-20, i-20, i-20);
-					init_color(COLOR_RED, i-40, i-40, i-40);
-					init_color(COLOR_YELLOW, i-60, i-60, i-60);
-				
-					r1.print(screen, cr1, 4);
-					r2.print(screen, cr2, 3);
-					r3.print(screen, cr3, 2);
-					r4.print(screen, cr4, 1);
-	 			}
-			} choice=1; break;
-		}
-	}
-*/	
 	return 0;
 }
-
